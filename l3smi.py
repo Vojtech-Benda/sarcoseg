8b5547e12ba65f4f867f35eb7c47e58d13a8749f
Vojtech-Benda <vojtech.benda@atlas.cz>
2025-07-28 14:10:01 +0200

    added preprocessing

---
+++ l3smi.py
@@ -0,0 +1,174 @@
+import sys
+import argparse
+import os
+from pathlib import Path
+from time import perf_counter
+from totalsegmentator.python_api import totalsegmentator
+import SimpleITK as sitk
+from nnunet.inference import predict
+
+from src.preprocessing import preprocess_dicom
+
+def get_args():
+    parser = argparse.ArgumentParser(prog="l3smi", description="segmentation of L3 axial tissues")
+    
+    sub_parsers = parser.add_subparsers(dest="command", help="select command to run")
+    
+    preprocess_parser = sub_parsers.add_parser("preprocess",
+                                               help="preprocess DICOM files and save as NifTi format",
+                                               description="preprocessing options")
+    preprocess_parser.add_argument("-i", 
+                                   "--input_dir",
+                                   type=str, 
+                                   help="path to DICOM files",
+                                   required=True)
+    preprocess_parser.add_argument("-o", 
+                                   "--output_dir",
+                                   type=str, 
+                                   help="path to save preprocessed NifTi files", 
+                                   default="./inputs")
+    preprocess_parser.add_argument("--anonymize", 
+                                   action="store_true", 
+                                   help="anonymize DICOM series before saving")
+    
+    dicom_tags = ("PatientSex", "PatientAge", "PatientHeight")
+    preprocess_parser.add_argument("--dicom_tags", 
+                                   nargs="+", 
+                                   help=f"space separated list of additional DICOM tags to extract (default: {dicom_tags})")    
+    segment_parser = sub_parsers.add_parser("segment", 
+                                            help="segment muscle and fat tissue at L3 level in axial viewl",
+                                            description="segmentation options")
+    segment_parser.add_argument("-i",
+                                "--input_dir",
+                                type=str,
+                                help="path to NifTi data to segment",
+                                default="./inputs")
+    segment_parser.add_argument("-o",
+                                "--output_dir",
+                                type=str, 
+                                help="path to output directory",
+                                default="./outputs")
+    segment_parser.add_argument("--volume_slices",
+                                type=int, 
+                                help=("number of slices in superior AND inferior direction from centroid index to extract for tissue segmentation\n"
+                                      "example: volume_slices=10, extract [centroid_index - 10:centroid_index + 10], segmentation over 20 slices"),
+                                default=0)
+    segment_parser.add_argument("--add_metrics",
+                                nargs="+",
+                                help="space separated list of additional metrics to compute", 
+                                metavar="metrics")
+    segment_parser.add_argument("--save_segmentations", 
+                                action="store_true", 
+                                help="save segmentation masks")
+
+    return parser.parse_args()
+
+""""
+def main():
+    in_dir = Path("./outputs/03.nii.gz")
+    seg_out = Path("./outputs/03_temp.nii")
+
+    # os.makedirs(out_dir, exist_ok=True)
+
+    target_vertebrae_map = {
+        "vertebrae_L1": 31, 
+        "vertebrae_L2": 30, 
+        "vertebrae_L3": 29, 
+        "vertebrae_L4": 28, 
+        "vertebrae_L5": 27, 
+        "vertebrae_S1": 26
+        }
+    print(f"targetting {target_vertebrae_map}")
+
+
+    times = {}
+    if not seg_out.exists():
+        print("starting total segmentator")
+        start = perf_counter()
+        totalsegmentator(input=in_dir, output=seg_out, fast=False, ml=True, quiet=False, task="total", roi_subset=list(target_vertebrae_map.keys()), device="gpu")
+        duration = perf_counter() - start
+        times['spine_seg'] = duration
+        print(f"segmentator finished in {duration}s")
+    else:
+        print("segmentation image exists")
+
+    seg_image: sitk.Image = sitk.ReadImage(seg_out) == target_vertebrae_map['vertebrae_L3']
+    # seg_image = sitk.Flip(seg_image, (False, False, True))
+    dicom_image: sitk.Image = sitk.ReadImage(in_dir)
+    
+    label_filter = sitk.LabelShapeStatisticsImageFilter()
+    label_filter.Execute(seg_image)
+    print(label_filter.GetLabels())
+    
+    l3_centroid = label_filter.GetCentroid(1)
+    l3_centroid_idx = seg_image.TransformPhysicalPointToIndex(l3_centroid)
+    print(f"phys {l3_centroid} idx {l3_centroid_idx}")
+
+
+    seg_image_sagittal = seg_image[l3_centroid_idx[0], ...]
+    l3_components_image = sitk.ConnectedComponent(seg_image_sagittal)
+    label_filter.Execute(l3_components_image)
+    
+    l3_body_label = max(label_filter.GetLabels(), key=lambda l: label_filter.GetNumberOfPixels(l))
+
+    l3_body: sitk.Image = l3_components_image == l3_body_label
+    
+    label_filter.Execute(l3_body)
+    l3_body_centroid = label_filter.GetCentroid(1)
+    l3_body_centroid_idx = l3_body.TransformPhysicalPointToIndex(l3_body_centroid)
+
+    l3_slice_idxs = (l3_body_centroid_idx[1] - 15, l3_body_centroid_idx[1] + 15)
+    l3_slices = dicom_image[..., l3_slice_idxs[0]:l3_slice_idxs[1]]
+    l3_slices_path = Path("outputs", "l3_slices_win.nii.gz")
+    sitk.WriteImage(l3_slices, l3_slices_path)
+    
+    target_tissues = {
+        "muscle": 1, 
+        "sat": 2, 
+        "vat": 3, 
+        "imat": 4
+        }
+    
+    l3_preds_path = Path("outputs", "l3_slices_preds_win.nii.gz")
+    model_dir = Path("models", "muscle_fat_tissue_stanford_0_0_2")
+    start = perf_counter()
+    f = predict.predict_cases(model=str(model_dir), 
+                          list_of_lists=[[l3_slices_path]], 
+                          output_filenames=[l3_preds_path],
+                          folds="all",
+                          save_npz=False,
+                          num_threads_preprocessing=8,
+                          num_threads_nifti_save=8,
+                          segs_from_prev_stage=None,
+                          do_tta=False,
+                          mixed_precision=True,
+                          overwrite_existing=True,
+                          all_in_gpu=False,
+                          step_size=0.5,
+                          checkpoint_name="model_final_checkpoint",
+                          segmentation_export_kwargs=None,
+                          disable_postprocessing=True
+                          )
+    duration = perf_counter() - start
+    print(f"tissue seg finished in {duration}s")
+    times['tissue_seg'] = duration
+    print(times, sum(times.values()))
+"""
+
+
+def segment_data(args: argparse.ArgumentParser):
+    raise NotImplementedError
+
+
+if __name__ == "__main__":
+    
+    args = get_args()
+    
+    if args.command == "preprocess":
+        preprocess_dicom(args.input_dir, args.output_dir, anonymize=args.anonymize, dicom_tags=args.dicom_tags)
+    elif args.command == "segment":
+        print("running segmentation")
+        print(args)
+    else:
+        print("unknown command")
+        sys.exit(-1)    
---
+++ src/preprocessing.py
@@ -0,0 +1,273 @@
+import sys
+import re
+
+from pathlib import Path
+from typing import Union, Optional
+from dataclasses import dataclass
+
+# image processing packages
+import pydicom
+from pydicom import datadict
+import SimpleITK as sitk
+import pandas as pd
+
+
+SERIES_DESC_PATTERN = re.compile(r'|'.join(
+    ("protocol", "topogram", "scout", "patient", "dose", "report")
+    ), re.IGNORECASE)
+CONTRAST_PHASES = re.compile(r'|'.join(
+    ("arterial", "nephro", "venous")
+    ), re.IGNORECASE)
+
+
+@dataclass
+class SeriesMetadata:
+    patient_id: str = None
+    study_instance_uid: str = None
+    study_date: str = None
+    series_instance_uid: str = None
+    series_description: str = None
+    filepaths: list[Union[str, Path]] = None
+    num_of_files: int = None
+    slice_thickness: Union[float, None] = None
+    has_contrast: bool = False
+    contrast_phase: str = None
+    kilo_voltage_peak: Union[float, None] = None
+    
+    additional_tags = {}
+    
+    def print_data(self, print_additional_tags=False):
+        msg = (
+            f"patient id: {self.patient_id}\n"
+            f"series description: {self.series_description}\n"
+            f"number of files: {self.num_of_files}\n"
+            f"slice thickness: {self.slice_thickness}\n"
+            f"has contrast: {self.has_contrast}\n"
+            f"contrast_phase: {self.contrast_phase}\n"
+            f"kilovoltage peak: {self.kilo_voltage_peak}\n"
+            )
+        print(msg)
+        
+        if print_additional_tags:
+            print(f"additional DICOM tags:\n{self.additional_tags}\n")
+
+
+def sort_files_by_series_uid(root_dir: Path, filepaths: list[str]) -> dict[str, list[str]]:
+    files_by_uid: dict[str, list[str]] = {}
+    
+    first_file = pydicom.dcmread(Path(root_dir, filepaths[0]))
+    patient_id = first_file.PatientID
+    
+    for path in filepaths:
+        dicom_path = Path(root_dir, path)
+        dataset = pydicom.dcmread(dicom_path)
+        series_uid = dataset.SeriesInstanceUID
+        
+        if series_uid in files_by_uid:
+            files_by_uid[series_uid].append(dicom_path)
+        else:
+            files_by_uid[series_uid] = [dicom_path]
+    
+    print(f"\nid: '{patient_id}' has {len(filepaths)} files across {len(files_by_uid.keys())} series")
+    return files_by_uid
+
+
+def filter_series_to_segment(all_series: dict, 
+                             additional_dicom_tags: Optional[list[str]] = None) -> Union[list[SeriesMetadata], None]:
+    """
+    Filter series by checking DICOM tags. Finds the native CT series with the lowest slice thickness and highest number of slices.
+    Also finds the contrast phase CT series with the lowest slice thickness and highest number of slices per each contrast phase found.
+    
+    Args:
+        dataset (pydicom.FileDataset): all series found.
+    
+    Returns:
+        list: list consisting of SeriesMetadata dataclass for native CT and each contract phase CT.
+    """
+    
+    contrast_series: dict[str, list[SeriesMetadata]] = {
+        'arterial': [],
+        'nephro': [],
+        'venous': []
+        }
+    native_series = []
+    
+    for _, filepaths in all_series.items():
+        
+        # read only the first file to filter series
+        # first_filepath = Path(root_dir, filepaths[0])
+        first_filepath = filepaths[0]
+        if not first_filepath.exists():
+            raise FileNotFoundError(2, "file not found", first_filepath)
+        dataset = pydicom.dcmread(first_filepath)
+        
+        # filter by words in SeriesDescription
+        series_desc: str = dataset.SeriesDescription
+        if SERIES_DESC_PATTERN.search(series_desc):
+            continue
+        
+        
+        slice_thickness = float(dataset.SliceThickness) if hasattr(dataset, "SliceThickness") else None
+        if slice_thickness is None:
+            continue
+        
+        contrast_applied = dataset.get("ContrastBolusAgent", None)
+
+        series_data = SeriesMetadata(
+            patient_id=dataset.PatientID,
+            study_instance_uid=dataset.StudyInstanceUID,
+            study_date=dataset.StudyDate,
+            series_instance_uid=dataset.SeriesInstanceUID,
+            series_description=dataset.SeriesDescription,
+            filepaths=filepaths,
+            num_of_files=len(filepaths),
+            slice_thickness=slice_thickness,
+            has_contrast=True if contrast_applied else False,
+            kilo_voltage_peak=int(dataset.get("KVP", None))
+            )
+        
+        # contrast_phase = series_desc.split(" ")[0].lower()
+        # contrast_phase = series_desc
+        contrast_match = CONTRAST_PHASES.search(series_desc)
+        if contrast_match:
+            series_data.contrast_phase = contrast_match.group().lower()
+        else:
+            series_data.contrast_phase = "none"
+        
+        # add additional tags for extraction
+        if additional_dicom_tags is not None:
+            for tag in additional_dicom_tags:
+                tag_value = dataset.get(tag, None)
+                series_data.additional_tags[tag] = tag_value
+        
+        # separate into contrast and no contrast
+        if series_data.has_contrast and series_data.contrast_phase != "none":
+            contrast_series[series_data.contrast_phase].append(series_data)
+        else:
+            native_series.append(series_data)
+            
+    # select the series with lowest slice thickness AND highest number of slices
+    selected_native_series = min(native_series, key=lambda v: (v.slice_thickness, -v.num_of_files))
+    
+    # same thing but for each contrast phase key - {'arterial': [...], 'nephro': [...], 'venous': [...]}
+    # skips empty lists
+    selected_contrast_series_by_phase = {
+        phase: min(series_list, key=lambda v: (v.slice_thickness, v.num_of_files))
+        for phase, series_list in contrast_series.items() if series_list
+    }
+
+    # return as a single list of selected series to segment
+    return [selected_native_series] + list(selected_contrast_series_by_phase.values())
+    
+
+def write_dicom_tags(path_df_dicom_tags: Union[Path, str], data: SeriesMetadata, dicom_tags: list) -> int:
+    df_dicom_tags = pd.read_csv(path_df_dicom_tags, header=0, index_col=0)
+
+    last_row_index = len(df_dicom_tags)
+
+    # start patient indexes at 1
+    pseudoname = f"sarco_{last_row_index + 1}"
+    
+    """
+    cols: [
+        "patient_id",
+        "pseudoname",
+        "study_instance_uid",
+        "study_date",
+        "series_description",
+        "slice_thickness",
+        "has_contrast",
+        "contrast_phase",
+        "kilo_voltage_peak"
+        ] + additional_dicom_tags
+    """
+    df_dicom_tags.loc[last_row_index] = [
+        data.patient_id,
+        pseudoname,
+        data.study_instance_uid,
+        data.study_date,
+        data.series_description,
+        data.slice_thickness,
+        data.has_contrast,
+        data.contrast_phase,
+        data.kilo_voltage_peak
+    ] + [data.additional_tags[tag] for tag in dicom_tags]
+    
+    df_dicom_tags.to_csv(path_df_dicom_tags, columns=df_dicom_tags.columns, header=True, index=True, index_label="index")
+    print(f"id: '{data.patient_id}' '(pseudoname: {pseudoname})' contrast_phase: '{data.contrast_phase}' written to csv")
+    return pseudoname
+
+
+def preprocess_dicom(input_dir: Union[str, Path], output_dir: Union[str, Path] = "./inputs", **kwargs):
+    print("preprocessing dicom files")
+    
+    # check if all additional tags are valid
+    additional_dicom_tags = kwargs.get("dicom_tags", None)
+    if additional_dicom_tags:
+        invalid_tags = [tag for tag in additional_dicom_tags if not datadict.dictionary_has_tag(tag)]
+        if invalid_tags:
+            print(f"wrong dicom tag name(s): {', '.join(map(str, invalid_tags))}")
+            sys.exit(-1)
+        
+    anonymize = kwargs.get("anonymize", False)
+        
+    input_dir = Path(input_dir)
+
+    # prepare table to output dicom tags
+    df_cols = [
+        "patient_id",
+        "pseudoname",
+        "study_instance_uid",
+        "study_date",
+        "series_description",
+        "slice_thickness",
+        "has_contrast",
+        "contrast_phase",
+        "kilo_voltage_peak"
+        ] + additional_dicom_tags
+    patient_tags_df = pd.DataFrame([], columns=df_cols)
+    patient_tags_df_path = Path(output_dir, "patient_dcm_tags.csv")
+    patient_tags_df.to_csv(patient_tags_df_path, columns=df_cols, header=True, index=True, index_label="index")
+
+    # SimpleITK reader to write series as NifTi format
+    sitk_reader = sitk.ImageSeriesReader()
+    sitk_reader.MetaDataDictionaryArrayUpdateOn()
+    sitk_reader.LoadPrivateTagsOn()
+
+    for root, _, files in input_dir.walk():
+        if not files or "DICOMDIR" in files:
+            continue
+        
+        files_by_series_uid = sort_files_by_series_uid(root, files)
+        
+        series_to_segment = filter_series_to_segment(files_by_series_uid, 
+                                                     root_dir=root, 
+                                                     additional_dicom_tags=additional_dicom_tags)
+        print(f"found {len(series_to_segment)} valid series for segmentation")
+        
+        for data in series_to_segment:
+            dataset = pydicom.dcmread(data.filepaths[0])
+            image_orientation_patient = dataset.ImageOrientationPatient
+            
+            sitk_reader.SetFileNames(data.filepaths)
+            sitk_image = sitk_reader.Execute()
+            
+            if image_orientation_patient != [1, 0, 0, 0, 1, 0]:
+                sitk_image: sitk.Image = sitk.DICOMOrient(sitk_image, "LPS")
+            
+            pseudoname = write_dicom_tags(patient_tags_df_path, data, additional_dicom_tags)
+            
+            if anonymize:
+                sitk_image.SetMetaData("0010|0010", pseudoname)
+            
+            nifti_filename = f"{pseudoname}_{data.contrast_phase}" if data.has_contrast else pseudoname
+            output_path = Path(output_dir, f"{nifti_filename}.nii.gz")
+            sitk.WriteImage(sitk_image, output_path)
+            print(f"id: '{data.patient_id}' (pseudoname: '{pseudoname}') with contrast phase '{data.contrast_phase}' as '{output_path.name}' written to nifti")
+            
+            
+if __name__ == "__main__":
+    if len(sys.argv) < 1:
+        print("Missing input directory")
+        sys.exit(-1)
+    preprocess_dicom(sys.argv[1])
