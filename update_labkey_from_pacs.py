f0d5c92cd0a18a67a2960ec2d8a913e657a107f8
Vojtěch-Benda <vojtech.benda@atlas.cz>
2026-03-12 17:50:36 +0100

    improved script selecting/updating with specific commands, added time filter to pacs querying

--- update_labkey_from_pacs.py
+++ update_labkey_from_pacs.py
@@ -1,4 +1,5 @@
 import sys
+from argparse import ArgumentParser
 
 import pandas as pd
 from labkey.query import QueryFilter
@@ -9,101 +10,175 @@ from tqdm import tqdm
 
 from src.network import database, pacs
 
-study_root_qr_model_find = _QR_CLASSES.get("StudyRootQueryRetrieveInformationModelFind")
+
+def get_args():
+    parent_parser = ArgumentParser(add_help=False)
+    parent_parser.add_argument(
+        "--schema",
+        type=str,
+        help="table type",
+        choices=("lists", "dataset"),
+        default="lists",
+        required=True,
+    )
+    parent_parser.add_argument("--query", type=str, help="table name", required=True)
+
+    parser = ArgumentParser(
+        prog="update_labkey_from_pacs",
+        description="This script is intended to update columns in StudyInstanceUID and PACS_CISLO on Labkey for all CT studies.",
+    )
+
+    command_parsers = parser.add_subparsers(dest="command", help="labkey APi actions")
+    select_parser = command_parsers.add_parser(
+        "select", parents=[parent_parser], help="select data from Labkey"
+    )
+    select_parser.add_argument("--columns", help="column names", nargs="+")
+
+    update_parser = command_parsers.add_parser(
+        "update", help="update data on Labkey", parents=[parent_parser]
+    )
+    update_parser.add_argument(
+        "--filepath", help="path to table with data", required=True
+    )
+
+    return parser.parse_args()
 
 
-"""
-This script is intended to update columns in StudyInstanceUID and PACS_CISLO on Labkey for all CT studies.
-"""
+def select(api: database.LabkeyAPI, schema: str, query: str, columns: list[str]):
 
-columns = [
-    "ID",
-    "PARTICIPANT",
-    "RODNE_CISLO",
-    "CAS_VYSETRENI",
-    "PACS_CISLO",
-    "STUDY_INSTANCE_UID",
-]
+    study_root_qr_model_find = _QR_CLASSES.get(
+        "StudyRootQueryRetrieveInformationModelFind"
+    )
+    if not study_root_qr_model_find:
+        raise ValueError()
 
-labkey_api = database.LabkeyAPI.init_from_json()
-response = labkey_api.query.select_rows(
-    schema_name="lists",
-    query_name="RDG-CT-Sarko-All",
-    columns=",".join(columns),
-    max_rows=-1,
-    filter_array=[QueryFilter("study_instance_uid", "", QueryFilter.Types.IS_BLANK)],
-)
+    response = api.query.select_rows(
+        schema_name=schema,
+        query_name=query,
+        columns=",".join(columns),
+        max_rows=-1,
+        filter_array=[
+            QueryFilter("study_instance_uid", "", QueryFilter.Types.IS_BLANK)
+        ],
+    )
 
-if not response.get("rows", None):
-    print("no rows returned from labkey")
-    sys.exit(-1)
+    if not response.get("rows", None):
+        print("no rows returned from labkey")
+        sys.exit(-1)
 
-raw_rows = [
-    {key: val for key, val in r.items() if key in columns} | {"StudyDescription": ""}
-    for r in response.get("rows", None)
-]
-print(f"returned rows {len(raw_rows)}")
-
-if not raw_rows:
-    sys.exit(-1)
-
-pacs_api = pacs.PacsAPI.init_from_json()
-
-ae = AE(ae_title=pacs_api.aet)
-ae.add_requested_context(study_root_qr_model_find)
-
-assoc = ae.associate(pacs_api.ip, pacs_api.port, ae_title=pacs_api.aec)
-if not assoc.is_established:
-    print("can't establish PACS association")
-    sys.exit(-1)
-
-for row in tqdm(raw_rows, miniters=100):
-    ds = Dataset()
-    ds.QueryRetrieveLevel = "STUDY"
-    ds.PatientID = row["RODNE_CISLO"]
-    ds.StudyDate = row["CAS_VYSETRENI"].split(" ")[0].replace("-", "")
-    ds.ModalitiesInStudy = "CT"
-    ds.AccessionNumber = ""
-    ds.StudyInstanceUID = ""
-    ds.StudyDescription = ""
-
-    response = assoc.send_c_find(ds, study_root_qr_model_find)
-    success_resp = [msg_id for stat, msg_id in response if stat.Status == 0xFF00]
-
-    row["STUDY_INSTANCE_UID"] = [
-        resp.get("StudyInstanceUID", "unknown") for resp in success_resp
+    raw_rows = [
+        {key: val for key, val in r.items() if key in columns}
+        | {"StudyDescription": ""}
+        for r in response.get("rows", None)
     ]
-    row["StudyDescription"] = [
-        resp.get("StudyDescription", "unknown") for resp in success_resp
-    ]
+    print(f"returned rows {len(raw_rows)} from {query}")
 
-assoc.release()
-if assoc.is_released:
-    print("PACS association released")
+    if not raw_rows:
+        sys.exit(-1)
 
-single_studies = [r for r in raw_rows if len(r["STUDY_INSTANCE_UID"]) == 1]
-multiple_studies = [r for r in raw_rows if len(r["STUDY_INSTANCE_UID"]) > 1]
-print(f"# of patients with single study in queried date: {len(single_studies)}")
-print(f"# of patients with multiple studies in queried date: {len(multiple_studies)}")
+    pacs_api = pacs.PacsAPI.init_from_json()
 
-if len(single_studies) > 0:
-    df_single_studies = (
-        pd.DataFrame(single_studies)
-        .explode(["STUDY_INSTANCE_UID", "StudyDescription"])
-        .reset_index(drop=True)
+    ae = AE(ae_title=pacs_api.aet)
+    ae.add_requested_context(study_root_qr_model_find)
+
+    assoc = ae.associate(pacs_api.ip, pacs_api.port, ae_title=pacs_api.aec)
+    if not assoc.is_established:
+        print("can't establish PACS association")
+        sys.exit(-1)
+
+    for row in tqdm(raw_rows, miniters=100):
+        date, time = row["CAS_VYSETRENI"].split(" ")  # from format Y-m-d H:M:S
+        time = time.split(":")  # from H:M:S into [H, M, S] to filter by hour only
+
+        # extended tiem range to account for a few seconds up to few minute difference between study time on Labkey vs Pacs
+        # example, Labkey: 13:59:20.000 vs Pacs: 14:00:40.000
+        time_range = f"{int(time[0])}-{int(time[0]) + 1}"
+
+        ds = Dataset()
+        ds.QueryRetrieveLevel = "STUDY"
+        ds.PatientID = row["RODNE_CISLO"]
+        ds.StudyDate = date.replace("-", "")
+        ds.StudyDate = time_range
+        ds.ModalitiesInStudy = "CT"
+        ds.AccessionNumber = ""
+        ds.StudyInstanceUID = ""
+        ds.StudyDescription = ""
+
+        response = assoc.send_c_find(ds, study_root_qr_model_find)
+        success_resp = [msg_id for stat, msg_id in response if stat.Status == 0xFF00]
+
+        row["STUDY_INSTANCE_UID"] = [
+            resp.get("StudyInstanceUID", "n/a") for resp in success_resp
+        ]
+        row["StudyDescription"] = [
+            resp.get("StudyDescription", "n/a") for resp in success_resp
+        ]
+
+    assoc.release()
+    if assoc.is_released:
+        print("PACS association released")
+
+    single_studies = [r for r in raw_rows if len(r["STUDY_INSTANCE_UID"]) == 1]
+    multiple_studies = [r for r in raw_rows if len(r["STUDY_INSTANCE_UID"]) > 1]
+    print(f"# of patients with single study in queried date: {len(single_studies)}")
+    print(
+        f"# of patients with multiple studies in queried date: {len(multiple_studies)}"
     )
-    df_single_studies.to_csv("single_studies.csv", header=True, index=False, sep=",")
-else:
-    print("no studies in single_studies, writing to csv nothing")
 
-if len(multiple_studies) > 0:
-    df_multiple_studies = (
-        pd.DataFrame(multiple_studies)
-        .explode(["STUDY_INSTANCE_UID", "StudyDescription"])
-        .reset_index(drop=True)
+    if len(single_studies) > 0:
+        df_single_studies = (
+            pd.DataFrame(single_studies)
+            .explode(["STUDY_INSTANCE_UID", "StudyDescription"])
+            .reset_index(drop=True)
+        )
+        df_single_studies.to_csv(
+            f"single_studies_{query}.csv", header=True, index=False, sep=","
+        )
+    else:
+        print("no studies in single_studies, writing to csv nothing")
+
+    if len(multiple_studies) > 0:
+        df_multiple_studies = (
+            pd.DataFrame(multiple_studies)
+            .explode(["STUDY_INSTANCE_UID", "StudyDescription"])
+            .reset_index(drop=True)
+        )
+        df_multiple_studies.to_csv(
+            f"multiple_studies_{query}.csv", header=True, index=False, sep=","
+        )
+    else:
+        print("no studies in multiple_studies, writing to csv nothing")
+
+
+def update(api: database.LabkeyAPI, schema: str, query: str, filepath: str):
+
+    # FIXME: test detecting delimiters!!
+    df = pd.read_csv(
+        filepath,
+        sep="[,;]",
+        header=0,
+        index_col=None,
+        usecols=("PARTICIPANT", "ID", "STUDY_INSTANCE_UID"),
+        engine="python",
     )
-    df_multiple_studies.to_csv(
-        "multiple_studies.csv", header=True, index=False, sep=","
-    )
-else:
-    print("no studies in multiple_studies, writing to csv nothing")
+
+    rows = [{col: row[col] for col in df.columns} for _, row in df.iterrows()]
+    print(f"updating {query} with {len(rows)} rows")
+
+    response = api.query.update_rows(schema_name=schema, query_name=query, rows=rows)
+    print(response)
+
+
+if __name__ == "__main__":
+    args = get_args()
+    print(args)
+
+    labkey_api = database.LabkeyAPI.init_from_json()
+
+    schema = args.schema
+    query = args.query
+
+    if args.command == "select":
+        select(labkey_api, schema, query, args.columns)
+    elif args.command == "update":
+        update(labkey_api, schema, query, args.filepath)
