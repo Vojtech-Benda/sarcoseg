c78c1e7a3c9ab72ae5e6e37fc0aa0cd449ae4bcc
Vojtěch-Benda <vojtech.benda@atlas.cz>
2026-04-07 12:16:36 +0200

    improved C-MOVE call to download series filtered by SeriesDescription regex match and convolution kernel

--- src/network/pacs.py
+++ src/network/pacs.py
@@ -7,11 +7,13 @@ from pathlib import Path
 from typing import Self
 
 from pydicom import dcmread
+from pydicom.multival import MultiValue
 from pynetdicom.apps.echoscu import echoscu
 
 # from pynetdicom.apps.movescu import movescu
 # from src import slogger
 from src.io import read_json
+from src.utils import SERIES_DESC_PATTERN
 
 log = logging.getLogger("pacs")
 
@@ -54,9 +56,9 @@ class PacsAPI:
             "-k",
             "SeriesDescription",
             "-k",
-            "SeriesInstanceUID",
-            "-k",
             "ImageType",
+            "-k",
+            "ConvolutionKernel",
             "-k",
             f"StudyInstanceUID={study_inst_uid}",
             "-od",
@@ -68,14 +70,14 @@ class PacsAPI:
 
         response_files = Path(response_dir).glob("rsp*.dcm")
 
-        query_series = []
+        query_files = []
         for rsp in response_files:
             ds = dcmread(rsp)
 
-            series_desc = ds.get("SeriesDescription", "")
-            series_uid = ds.get("SeriesInstanceUID")
-            if series_desc and "dose report" in series_desc.lower():
-                query_series.append(series_uid)
+            series_desc = ds.get("SeriesDescription", "").lower()
+
+            if "dose report" in series_desc:
+                query_files.append(rsp)
                 continue
 
             image_type = ds.get("ImageType", [])
@@ -84,10 +86,19 @@ class PacsAPI:
             ):
                 continue
 
-            query_series.append(series_uid)
+            if convolution_kernel := ds.get("ConvolutionKernel", ""):
+                convolution_kernel = (
+                    convolution_kernel[0]
+                    if isinstance(convolution_kernel, MultiValue)
+                    else convolution_kernel
+                )
+                if "bl57" in convolution_kernel.lower():
+                    continue
 
-        # clean the response directory
-        shutil.rmtree(response_dir)
+            if SERIES_DESC_PATTERN.search(series_desc):
+                continue
+
+            query_files.append(rsp)
 
         args = [
             "./dcmtk/movescu",
@@ -106,13 +117,14 @@ class PacsAPI:
             "QueryRetrieveLevel=SERIES",
             "-k",
             f"StudyInstanceUID={study_inst_uid}",
-            "-k",
-            f"SeriesInstanceUID={'\\'.join(query_series)}",
             # TODO: add response files here!!!
-        ]
+        ] + query_files
 
         log.debug(f"running C-MOVE for StudyInstanceUID: {study_inst_uid}")
         result = subprocess.run(args, capture_output=True, text=True)
+
+        # clean the response directory
+        shutil.rmtree(response_dir)
 
         if len(os.listdir(download_directory)) == 0:
             os.rmdir(download_directory)
